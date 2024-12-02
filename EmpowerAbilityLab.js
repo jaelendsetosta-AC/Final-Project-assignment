824515aa4934b68e3e67ccf1b31238027da1daa2
Donna Ha <65718414+Donna-Smee@users.noreply.github.com>
2024-12-02 04:19:02 -0500

    Added Error Messaging Functionality for Schedule a Call

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -1,3 +1,10 @@
+const formMessageID = "formMessage";
+const scheduleFormID = "scheduleCallForm";
+
+const error = "Errors";
+const thankYou = "Thank you!"
+const errorLinkClass = "error-link";
+
 document.addEventListener("DOMContentLoaded", () => {
     // Function to handle tab switching
     const handleTabSwitch = (event) => {
@@ -47,3 +54,75 @@ document.addEventListener("DOMContentLoaded", () => {
     // Mark the Home tab as active
     document.querySelector(".nav-link[href='#home']").classList.add("active");
 });
+
+
+// When the schedule a call form submit button is activated
+document.getElementById(scheduleFormID).addEventListener('submit', function(event) {
+    // Preventing the default form submission behavior
+    event.preventDefault();
+
+    // validate all required fields are filled correctly
+    let listofErrors = [];
+    let validated = validateFormCall(listofErrors);
+
+    // show either thank you message or error message
+    if (validated){
+        sendMessageForm(thankYou, listofErrors);
+    }else {sendMessageForm(error, listofErrors);}
+
+
+});
+
+
+
+
+// Function: Validate all required fields for the "Schedule a Call" Page form
+function validateFormCall(listofErrors){
+    // get the email value
+    let email = document.getElementById("email").value.trim();
+    if (email === ""){
+        // no email, send error
+        listofErrors.push({"id": "email", "message": "Please enter your email address!"});
+        return false;
+    }
+    return true;
+
+}
+
+// clear an element off of the HTML page given the ID
+function clearElement(elementID){
+    $('#' + elementID).remove();
+}
+
+// Send either a thank you message or error messages to the user after the "Schedule a call" button is activated
+function sendMessageForm(fieldsetLegend, listofErrors){
+    // clear the fieldset first
+    clearElement(formMessageID);
+
+    // create a field set for the messages
+    let fieldsetHTML = '<fieldset id="' + formMessageID + '"><legend>' + fieldsetLegend + '</legend><ul></ul></fieldset>';
+    $('#'+scheduleFormID).prepend(fieldsetHTML);    
+
+
+    // traverse through each error to display it
+    // if there are no errors, the list will be empty
+    listofErrors.forEach(function (error){
+        let errorID = error.id;
+        let errorMessage = error.message;
+
+        // class of "error-link" is given so that we can customize the behaviour
+        let listItemHTML = '<li><a href="#' + errorID + '" class="error-link" >' + errorMessage + '</a></li>';
+        $('#' + formMessageID + ' ul').append(listItemHTML);
+    });
+
+
+    // add event listener for error links (using jquery)
+    $('.error-link').on('click', function(event) {
+        event.preventDefault();  
+        let targetId = $(this).attr('href').substring(1);  // Get the id of the target form field
+        $("#" + targetId).focus();
+    
+    });
+}
+
+
